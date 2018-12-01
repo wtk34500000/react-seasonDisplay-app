8f4dba677c59c4ddfac9c4827491e84ffc06a5b1
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2018-11-30 21:26:32 -0500

    season app

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import SeasonalDisplay from './SeasonDisplay'
+import Loader from './Loader'
 
 class App extends Component{
-    constructor(props){
-        super(props);
+    state = {
+        lat: null,
+        errorMessage: ''
+    };
 
-        this.state = {
-            lat: null,
-            errorMessage:''
-        }
-
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position)=>{
-                this.setState({ lat:position.coords.latitude })
-            },
-            (err) => {
-                this.setState({ errorMessage:err.message})
-            }
+            (position)=>this.setState({ lat:position.coords.latitude }),
+            (err) =>this.setState({ errorMessage:err.message})
         );
     }
 
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }else if(!this.state.errorMessage && this.state.lat){
+            return <SeasonalDisplay lat = {this.state.lat}/>
+        }else{
+            return <Loader message="Please accept the location request"/>
+        }
+    }
 
     render(){
-            if(this.state.errorMessage && !this.state.lat){
-                return <div>Error: {this.state.errorMessage}</div>;
-            }else if(this.state.lat && !this.state.errorMessage){
-                return <div>Latitude: {this.state.lat}</div>;
-            }else{
-                return <div>Loading....</div>;
-            }
+        return <div>{this.renderContent()}</div>
     }
     
 }
